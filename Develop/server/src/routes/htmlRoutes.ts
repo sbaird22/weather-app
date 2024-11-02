2be293cfb8b553f8382c104ae3be034b92c27aae
sbaird22 <shanebaird30@yahoo.com>
2024-11-01 19:03:26 -0600

    Start of history.ts and the htm route file

--- Develop/server/src/routes/htmlRoutes.ts
+++ Develop/server/src/routes/htmlRoutes.ts
@@ -7,4 +7,6 @@ const router = Router();
 
 // TODO: Define route to serve index.html
 
+router.get('/', (_req, res) => { res.sendFile(path.join(__dirname, 'index.html')); });
+
 export default router;
