0f9be195ef9ef75afbcb553445229eaf5fb630f4
sbaird22 <shanebaird30@yahoo.com>
2024-11-05 20:48:27 -0700

    updated the routes.ts file

--- Develop/server/src/routes/api/weatherRoutes.ts
+++ Develop/server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,41 @@
 import { Router, type Request, type Response } from 'express';
+//import { readFileSync } from 'fs';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
+
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', (req: Request, res: Response) => {
   // TODO: GET weather data from city name
+  try{
+    const cityName = req.body.cityName;
+WeatherService.getWeatherForCity(cityName).then((data)=> {
   // TODO: save city to search history
+  HistoryService.addCity(cityName);
+  res.json(data);
+})
+}catch(err){
+console.log(err);
+res.status(500).json(err);
+}
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get('/history', async (_req: Request, res: Response) => {
+  HistoryService.getCities()
+  .then((data)=> {
+return res.json(data);
+  })
+  .catch((err)=> {
+    res.status(500).json(err);
+});
+});
+  
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+//router.delete('/history/:id', async (req: Request, res: Response) => {});
 
 export default router;
