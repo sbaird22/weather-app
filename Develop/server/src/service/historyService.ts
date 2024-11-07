30cc0b81e9d93d5f36cc6284a0581e5204cbfa82
sbaird22 <shanebaird30@yahoo.com>
2024-11-06 19:46:39 -0700

    fixing errors

--- Develop/server/src/service/historyService.ts
+++ Develop/server/src/service/historyService.ts
@@ -1,9 +1,11 @@
+// TODO: Define a City class with name and id properties
+//import express from 'express';
 class City {
-  name: string;
-  id: number;
-  constructor(name: string, id: number) {
-    this.name = name;
-    this.id = id;
+  cityName: string;
+  // temperature: number;
+  constructor(cityName: string) {
+    this.cityName = cityName;
+    // this.temperature = temperature;
   }
 }
 
@@ -11,51 +13,62 @@ class City {
 import fs from 'fs';
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-    private async read() {
-      try {
-        const data = await fs.promises.readFile('searchHistory.json', 'utf-8');
-        const jsonData = JSON.parse(data);
-        return jsonData;
-      } catch (err) {
-        console.error('Error reading search history:', err);
-          return [];
-        } 
-      }
-    
-  // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  private async write(cities: City[]) {
+  ;
+   async read() {
     try {
-      const jsonData = JSON.stringify(cities,null,2);
-      await fs.writeFile('searchHistory.json', jsonData, function (err) {
-        if (err) {
-          throw err;
-        }}
-    )} catch (err) {
-      console.log('Error writing to searchHistory.json', err);
-    }
+
+      const data = await fs.promises.readFile('searchHistory.json', 'utf-8');
+      const jsonData = JSON.parse(data);
+      console.log('parsed data',jsonData);
+      return jsonData;   
+    } catch (err) {
+      console.error('Error reading search history:', err);
+      return [];
+    } 
   }
+  
+   async write(cities: City[] = []):Promise<void> {
+   try {
+  
+     const jsonData = JSON.stringify(cities);
+     console.log(jsonData);
+     fs.writeFile('searchHistory.json', jsonData, function (err) {
+       if (err) {
+         throw err;
+       }}
+   )} catch (err) {
+     console.log('Error writing to searchHistory.json', err);
+   }
+  }
+
+     
+  // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  async getCities() {
+   async getCities() {
+  
     try {
       const data = await this.read();
-    const cities: City[] = JSON.parse(data);
+    const cities = JSON.parse(data);
+    console.log('cities',cities);
     return cities;
     } catch (err) {
       console.error('Error reading cities:', err);
       return [];
-    }
-  }
+   }}
+  
   // TODO Define an addCity method that adds a city to the searchHistory.json file
   async addCity(city: string) {
     try {
-      const data = await this.read();
-      const searchHistory = JSON.parse(data);
-  
+      const cities = await this.read();
+      //const searchHistory = JSON.parse(data);
+      cities.push({ cityName: city });
+      await this.write(cities);
+      console.log(cities);
       // Check if the city already exists
-      if (!searchHistory.includes(city)) {
-        const updatedCities = searchHistory.push(city);
-        await this.write(updatedCities);
-        }
+      // if (!searchHistory.includes(city)) {
+      //   const updatedCities = searchHistory.push(city);
+      //   await this.write(updatedCities);
+      //   }
       } catch (err) {
       console.error('Error adding city:', err);
       throw err; 
@@ -65,5 +78,14 @@ class HistoryService {
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   // async removeCity(id: string) {}
 
-}
+    }
+ const histServ =  new HistoryService();
+//  const app = express();
+// app.use(express.json());
+histServ.write();
+  histServ.read();
+  histServ.getCities();
+  histServ.addCity('Denver')
+
+
 export default new HistoryService();
